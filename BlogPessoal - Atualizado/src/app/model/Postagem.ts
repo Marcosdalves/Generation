4c2eedf6825a03fc3ca15ffbacc2e0216dcf5abb
Marcosdalves <0marcosalves@gmail.com>
2022-03-30 00:51:47 -0300

    Consumo de API

---
+++ BlogPessoal - Atualizado/src/app/model/Postagem.ts
@@ -0,0 +1,13 @@
+import { tema } from "./Tema"
+import { User } from "./User"
+
+export class postagem{
+public id: number
+public titulo: string
+public texto: string
+public date: Date
+public usuario: User
+public tema: tema
+
+
+}
---
+++ BlogPessoal - Atualizado/src/app/model/Tema.ts
@@ -0,0 +1,8 @@
+import { postagem } from "./Postagem"
+
+export class tema{
+public id: number
+public descricao: string
+public postagem: postagem[]
+
+}
